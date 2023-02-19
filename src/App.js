f1114070766154903db6d6df17970269377ee43b
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-19 08:32:38 +0530

    feat- App.js classname changed to class

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="App">
+      <header class="App-header">
+        <img src={logo} class="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          class="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
