52ac81ccd33b5b2fb25c16ff43f9f65fa86a3427
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-19 08:36:36 +0530

    feat- applied styles with App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
+    <div className="App">
+      <h1>Hi</h1>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          class="App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
