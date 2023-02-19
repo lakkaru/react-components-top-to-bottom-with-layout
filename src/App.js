053923f8b5fabff6e85feb5d78f10e1b8fc14aec
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-19 13:17:02 +0530

    feat- props passing to the component

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ function App() {
       <p>Session 4</p>
       <p>Session 5</p>
       <p>Session 6</p>
+      <MyPara text='Session 7'/>
     </div>
   );
 }
 
 export default App;
+
+ function MyPara(props) {
+  // console.log(props.text);
+  
+  return <p style={{fontSize:'25px'}}>{props.text}</p>;
+ }
