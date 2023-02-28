375becb2e21bbe8f475289b4e204203fabef6e36
Lakshman Karunasena <lakkaru@gmail.com>
2023-02-28 22:33:03 +0530

    fix- left nav component right arrow set to right

--- src/components/ComponentSec/Components.js
+++ src/components/ComponentSec/Components.js
@@ -4,9 +4,9 @@ export default function Components({ leftImage, compo, rightImage }) {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
       <img src={leftImage} height={"15px"} />
-      <p style={{ padding: '10px' }}>{compo}</p>
-      <div style={{ textAlign:'right'}}>
-        <img src={rightImage} height={"10px"}  />
+      <p style={{ padding: "10px" }}>{compo}</p>
+      <div style={{ textAlign: "right", width: "100%" }}>
+        <img src={rightImage} height={"10px"} />
       </div>
     </div>
   );
